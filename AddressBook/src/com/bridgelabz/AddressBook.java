bd5c0ba18c379104f4a547c4bb3ccadc25bc119e
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-14 16:11:40 +0530

    Added

---
+++ AddressBook/src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System");
+		Info contact = new Info("Sai","Tarun","Guntur","Guntur","Andhra Pradeah",522508,"0123456789","tarun@gmail.com");
+		System.out.println(contact);
+	}
+
+}
